f0dcc90f1ab584c297390772ed59c4d413ad91c0
Alessandro <alessandro.dindinelli@gmail.com>
2020-05-16 15:00:05 +0200

    Aggiunto distruttore

--- docente.cpp
+++ docente.cpp
@@ -3,6 +3,8 @@
 Docente::Docente(std::string n, unsigned short e, unsigned short o, bool c)
     : Persona(n,e), Impiegato(n,e,o), cattedra(c) {}
 
+Docente::~Docente() {}
+
 unsigned short Docente::costo() const
 {
     return getOreLavoro() * pagaOraria + cattedra * bonusDocente;
