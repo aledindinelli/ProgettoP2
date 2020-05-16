08c67ed948ef98fa7814d5e16e83533d037992e5
Alessandro <alessandro.dindinelli@gmail.com>
2020-05-16 15:00:34 +0200

    Aggiunto distruttore

--- tecnico.cpp
+++ tecnico.cpp
@@ -3,6 +3,8 @@
 Tecnico::Tecnico(std::string n, unsigned short e, unsigned short o)
     : Persona(n,e), Impiegato(n,e,o) {}
 
+Tecnico::~Tecnico() {}
+
 unsigned short Tecnico::costo() const
 {
     return getOreLavoro() * pagaOraria;
